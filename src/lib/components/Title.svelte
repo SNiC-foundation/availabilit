80a80db88d0d2789ed6f95334616fa395947cc3b
dilanvoosten <dilanoosten@gmail.com>
2024-11-21 22:56:19 +0100

    fix(landing-page): #1 Removed the location from the Title

--- src/lib/components/Title.svelte
+++ src/lib/components/Title.svelte
@@ -50,7 +50,6 @@
 
     <div id="infoText" class="absolute bottom-16 w-full text-center text-xl sm:text-2xl lg:text-4xl font-medium">
         <h3>Tuesday 25th of November, 2025</h3>
-        <h3>Orpheus, Apeldoorn</h3>
     </div>
 
 </div>
