d15e2e387f427a83e1ee8ad179210a9056016783
Dilan van Oosten <dilanoosten@gmail.com>
2024-11-25 22:11:27 +0100

    Added new build folder

---
+++ build/_app/immutable/chunks/paths.BSaH2F6Y.js
@@ -0,0 +1,1 @@
+var s;const t=((s=globalThis.__sveltekit_wb7y50)==null?void 0:s.base)??"/SNiC-foundation/availibilit";var a;const i=((a=globalThis.__sveltekit_wb7y50)==null?void 0:a.assets)??t;export{i as a,t as b};
