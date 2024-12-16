30377f4d0e79e48764717b8d62f0ec9c99f2532c
Dilan van Oosten <dilanoosten@gmail.com>
2024-12-16 18:15:04 +0100

    feat(committee): #8 Made the committee part except for the personal pictures and the social media links

--- src/lib/components/Committee.svelte
+++ src/lib/components/Committee.svelte
@@ -1,5 +1,7 @@
 <script lang="ts">
-    import commiteePicture from "$lib/images/committeePicture.png";
+    import committeePicture from "$lib/images/committeePicture.png";
+    import Member from "$lib/components/Member.svelte";
+    import { base } from "$app/paths";
 
     const members = [
         {name: "Matthias Wentink", functions: ["Chairman", "Public Relations"]},
@@ -9,34 +11,40 @@
         {name: "Mariska Frelier", functions: ["Commissioner of External Affairs"]},
         {name: "Bram Rutgers", functions: ["Commissioner of Speakers"]},
         {name: "Hayel Akel", functions: ["Commissioner of Speakers"]},
-        // {name: "Anne van Esch", functions: ["Association Board Responsible"]},
-        // {name: "Jarek Wiedemeijer", functions: ["Association Board Responsible"]},
+        {name: "Anne van Esch", functions: ["Association Board Representative"]},
+        {name: "Jarek Wiedemeijer", functions: ["Association Board Representative"]},
     ];
 </script>
 
-<div id="committee_section">
-    <h1 class="sectionTitle">Committee</h1>
-
-    <p>This is the SNiCc 2025; AvailaibilIT</p>
-
+<div id="committee_section" class="p-8 max-w-full">
+    <div id="title" class="flex flex-col gap-3 items-center mb-5 md:ml-24 md:items-start">
+        <h1 class="sectionTitle">Committee</h1>
+        <p>This is the SNiC Committee 2025; <a href="{base}/" class="text-blue-whale"><i>AvailabilIT</i></a></p>
+    </div>
     <div id="committee_picture">
-        <img src={commiteePicture} alt="" />
+        <img src={committeePicture} alt=""/>
+        <p id="altText" class="pl-2 pt-1 text-xs md:text-[1rem]"><i>f.l.t.r</i> : {members[6].name}, {members[4].name}, {members[2].name}
+            , {members[0].name}, {members[1].name},
+            {members[3].name}, {members[5].name}</p>
     </div>
 
-    <div id="members_pictures" class="container">
-        {#each members as member}
-            <div class="member">
-                <div class="member_picture"></div>
-                <div class="member_info">
-                    <h2>{member.name}</h2>
-                    <ul>
-                        {#each member.functions as fun}
-                            <li>{fun}</li>
-                        {/each}
-                    </ul>
-                </div>
-            </div>
-        {/each}
+
+    <div id="members" class="container flex flex-col">
+        <div id="member_row_one" class="memberRow">
+            {#each members.slice(0, 3) as member}
+                <Member member={member}/>
+            {/each}
+        </div>
+        <div id="member_row_two" class="memberRow">
+            {#each members.slice(3, 7) as member}
+                <Member member={member}/>
+            {/each}
+        </div>
+        <div id="member_row_three" class="memberRow">
+            {#each members.slice(7) as member}
+                <Member member={member}/>
+            {/each}
+        </div>
 
     </div>
 </div>
---
+++ src/lib/components/Member.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+    export let member: {name: string, functions: string[]};
+</script>
+
+
+<div id="member" class="memberContainer">
+    <div id="picture">
+
+    </div>
+    <div id="info" class="text-wrap text-center text-blue-whale">
+        <h3 class="font-bold">{member.name}</h3>
+        {#if member.functions.length > 1}
+            <h3><i>{member.functions[0]} & <br> {member.functions[1]}</i></h3>
+        {:else}
+            <h3><i>{member.functions[0]}</i></h3>
+        {/if}
+    </div>
+    <div id="socials">
+
+    </div>
+
+</div>
