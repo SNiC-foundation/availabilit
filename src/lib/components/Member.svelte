04d27e29983e01bd040afe9d9fdc89cf588f096d
Dilan van Oosten <dilanoosten@gmail.com>
2024-12-28 15:44:31 +0100

    feat(committee): #8 Added example picture to help with formatting them on the website

--- src/lib/components/Member.svelte
+++ src/lib/components/Member.svelte
@@ -1,11 +1,14 @@
 <script lang="ts">
+    import picture from "$lib/images/IMG_8972.jpg"
+
     export let member: {name: string, functions: string[]};
+
 </script>
 
 
 <div id="member" class="memberContainer">
     <div id="picture">
-
+        <img src={picture} alt="Picture of {member.name}" />
     </div>
     <div id="info" class="text-wrap text-center text-blue-whale">
         <h3 class="font-bold">{member.name}</h3>
