f25ee4d6da3b92a793e062fad59d1df36d79446d
Dilan van Oosten <dilanoosten@gmail.com>
2025-01-30 10:40:57 +0100

    Made the part for Accessibility a separate subsection a

---
+++ src/lib/components/Accessibility.svelte
@@ -0,0 +1,28 @@
+ <div id="accessibility_section" class="container flex-col text-blue-whale p-8 gap-4 text-center md:text-left">
+
+     <h2 class="text-blue-whale text-2xl md:text-5xl font-bold">Accessibility</h2>
+
+     <div class="flex flex-row w-full lg:max-h-[70vh] gap-4 text-blue-whale">
+
+         <article class="lg:w-1/2">
+             At SNiC, we are dedicated to ensuring that our event is inclusive for all attendees. We strive to
+             accommodate various needs and provide a welcoming environment for everyone. If you have any questions or
+             specific accessibility requests, please do not hesitate to reach out to us. We are committed to making
+             your experience as comfortable and enjoyable as possible.
+         </article>
+
+
+         <!-- The contact person box  -->
+         <div id="wrapper" class="lg:w-1/2 bg-start text-white flex flex-col gap-4 p-4">
+             
+             <h3 class="text-4xl">Questions</h3>
+         
+             <div id="email">
+                 <p>For questions you can reach out to:</p>
+                 <a href="mailto:accessibility@availabilit.snic.nl">accessibility@availabilit.snic.nl</a>
+             </div>
+         
+         </div>
+     </div>
+ </div>
+
