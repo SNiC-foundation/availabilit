49f8beacab3d658df07e4b23c6cfe01864a2a57d
Dilan van Oosten <dilanoosten@gmail.com>
2025-03-07 15:43:50 +0100

    feat(location): #26 Added the boiler plate for the location section
    Just have to change the text and image when I gte something back from Danielle

---
+++ src/lib/components/Location.svelte
@@ -0,0 +1,32 @@
+<script>
+    import Test from "$lib/images/orpheus.jpeg";
+</script>
+
+
+<div id="location_section" class="bg-gray-200 flex flex-col w-full content-around pt-[5vh]">
+
+    <div class="container flex-col text-blue-whale p-8 gap-4 text-center md:text-left ">
+
+        <h1 class="text-blue-whale text-2xl md:text-5xl font-bold">Location</h1>
+
+
+        <div id="text-and-image" class="flex flex-col lg:flex-row w-full lg:h-max-[70vh] gap-8 lg:gap-4 text-blue-whale">
+
+            <article class="lg:w-1/2">
+                Orpheus is het grootste culturele podium van Apeldoorn en de wijde regio. 
+                Wij presenteren een breed, aantrekkelijk en deels vernieuwend en exclusief aanbod aan 
+                theater en muziek voor een breed en divers publiek uit Apeldoorn en omstreken. Ons theater 
+                biedt een gastvrij podium aan creatieve initiatieven van binnen en buiten de stad. Wij zijn
+                 daardoor een belangrijke culturele ontmoetingsplek, waar het publiek gastvrij wordt 
+                 ontvangen en het zich door ons laat inspireren, uitdagen en vermaken. Daarnaast verwelkomen 
+                 wij zakelijke evenementen met excellente service en dragen daarmee niet alleen bij aan 
+                 de eigen gezonde exploitatie, maar ook aan de economische bedrijvigheid en positionering
+                  van Apeldoorn.
+            </article>
+
+            <img src={Test} alt="" class="lg:w-1/2"/>
+
+        </div>
+    </div>
+    
+</div>
