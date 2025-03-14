16e685c37a9b59652b02ff694d0f8f32859541f2
Dilan van Oosten <dilanoosten@gmail.com>
2025-03-14 16:07:51 +0100

    feat(location): #26 Added Location to the NavBar

--- src/lib/components/Navbar.svelte
+++ src/lib/components/Navbar.svelte
@@ -12,11 +12,12 @@
 <nav class="navbar flex w-screen h-16 fixed top-0 z-10 backdrop-blur-md items-center" style="background: rgba(0,50,73,{Math.min(scroll / height,1)})">
     <div class="container sm:px-8 justify-between items-center">
         <a href="{base}/" class="h-12 hidden sm:block"><img src={WordMark} alt="" class="h-full"></a>
-        <div class="flex flex-row items-center sm:gap-4">
+        <div class="flex flex-row items-center flex-wrap sm:gap-4">
             <Navbutton text="Home" section="#landing_section"/>
             <Navbutton text="Theme" section="#theme_section"/>
             <Navbutton text="About" section="#about_section"/>
             <Navbutton text="Accessibility" section="#accessibility_section"/>
+            <Navbutton text="Location" section="#location_section"/>
         </div>
     </div>
 </nav>
