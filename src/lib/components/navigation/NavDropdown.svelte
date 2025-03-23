9e42770f2c565ab18b06b3f706f4f0ba87fc99d4
Dilan van Oosten <dilanoosten@gmail.com>
2025-03-23 22:50:22 +0100

    feat(navigation): #27 Added first version of the dropdwown of which the background does scale color together with the header

--- src/lib/components/navigation/NavDropdown.svelte
+++ src/lib/components/navigation/NavDropdown.svelte
@@ -6,16 +6,45 @@
     export let scroll:number;
     export let height:number;
 
+    let isDropDown:boolean = false;
+
+    const handleDropdown = () => {
+        isDropDown = !isDropDown;
+    }
+
+const handleNavbuttonClick = () => {
+        isDropDown = false;
+    }
+
+    $: dropdownStyle = `display: ${isDropDown ? 'flex' : 'none'};
+     top: ${Math.max(64, Math.min(scroll / height, 1) * 64)}px;
+     background: rgba(0,50,73,${Math.min(scroll / height,1)});`;
+   
+
 
 </script>
 
 <nav class="navbar flex w-screen h-16 fixed top-0 z-10 backdrop-blur-md items-center" style="background: rgba(0,50,73,{Math.min(scroll / height,1)})">
     <div class="container justify-around items-center">
         <a href="{base}/" class="h-12"><img src="{WordMark}" alt="" class="h-full"></a>
-        <div id=dropdown class="text-picton-blue"><i class="fa-solid fa-bars"></i></div>
-
+        <button id=dropdown class="text-picton-blue" on:click={handleDropdown}>
+            <!-- Switch between the icons -->
+            {#if isDropDown}
+                <i class="fa-solid fa-bars-staggered"></i>
+            {:else}
+                <i class="fa-solid fa-bars"></i>
+            {/if}
+        </button>
+    </div>
 </nav>
-
+<!-- Dropdown content -->
+<div class="flex-col items-center gap-3 h-fit w-full fixed z-10 size-10 backblue-whale"
+         style={dropdownStyle}>
+    <Navbutton text="Home" section="#landing_section" />
+    <Navbutton text="Theme" section="#theme_section"  />
+    <Navbutton text="About" section="#about_section"  />
+    <Navbutton text="Accessibility" section="#accessibility_section" />
+</div>
 
 <style>
     .navbar {
