e2d61db487ccfe3042783bdeb0b0a94f2a4df5ff
MatthiasWentink <matthiaswentink27@gmail.com>
2025-04-30 11:30:34 +0200

    install 'marked'

--- src/routes/speakers/+page.svelte
+++ src/routes/speakers/+page.svelte
@@ -5,17 +5,17 @@
    * @type {string | any[]}
    */
   const speakers = [
-    // {\
-    //     image: "https://celerit.snic.nl/speakers/c96dc266-99f5-44d6-ba68-3823abb1de1c.png",
-    //     name: "John Doe",
-    //     description: "John Doe is a seasoned expert in technology and innovation. He has over 20 years of experience in the field.",
-    //     extendedDescription: "John Doe has been at the forefront of technological advancements, contributing to numerous groundbreaking projects that have shaped the industry. His expertise spans across various domains including artificial intelligence, machine learning, and blockchain technology. John has been a keynote speaker at several international conferences, sharing his insights and experiences with a global audience.",
-    //     tags: ['Technology', 'Innovation', 'Philosophy'],
-    //     links: [
-    //         {icon: "fa-brands fa-linkedin", url: "https://www.linkedin.com/in/johndoe/"},
-    //         {icon: "fa-brands fa-twitter", url: "https://twitter.com/johndoe"}
-    //     ]
-    // }
+    {
+        image: "https://celerit.snic.nl/speakers/c96dc266-99f5-44d6-ba68-3823abb1de1c.png",
+        name: "John Doe",
+        description: "John Doe is a seasoned expert in technology and innovation. He has over 20 years of experience in the field.",
+        extendedDescription: "John Doe has been at the forefront of technological advancements, contributing to numerous groundbreaking projects that have shaped the industry. His expertise spans across various domains including artificial intelligence, machine learning, and blockchain technology. John has been a keynote speaker at several international conferences, sharing his insights and experiences with a global audience.",
+        tags: ['Technology', 'Innovation', 'Philosophy'],
+        links: [
+            {icon: "fa-brands fa-linkedin", url: "https://www.linkedin.com/in/johndoe/"},
+            {icon: "fa-brands fa-twitter", url: "https://twitter.com/johndoe"}
+        ]
+    }
   ]
 </script>
 <Header title="Speakers"/>
