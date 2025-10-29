7d703ca727b08374db28857f5ce55215f7f535d1
MatthiasWentink <matthiaswentink27@gmail.com>
2025-10-29 11:00:37 +0100

    Fix for optional description

--- src/routes/partners/CompanyCard.svelte
+++ src/routes/partners/CompanyCard.svelte
@@ -32,8 +32,8 @@
             </a>
         </div>
         <div class="px-3 md:px-6 py-3 prose prose-sm md:prose-base prose-headings:text-blue-whale prose-headings:m-0 prose-h1:text-2xl prose-ul:my-0 prose-p:mt-0 max-w-none ">
-            <p class="text-ellipsis transition-all duration-200 {showMore ? 'h-50' : 'h-28 overflow-hidden line-clamp-4'}">{@html marked(company.description)}</p>
             {#if company.description && typeof company.description === 'string' && company.description.length > 0}
+                <p class="text-ellipsis transition-all duration-200 {showMore ? 'h-50' : 'h-28 overflow-hidden line-clamp-4'}">{@html marked(company.description)}</p>
                 <button on:click={() => showMore = !showMore} class="text-blue-500 mt-2">
                     {#if showMore} Show Less {:else} Show More {/if}
                 </button>
