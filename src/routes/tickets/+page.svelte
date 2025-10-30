9f26e93650b1e1931cd144a4863f7dca698cfb84
MatthiasWentink <matthiaswentink27@gmail.com>
2025-10-30 11:13:18 +0100

    Ticket table height

--- src/routes/tickets/+page.svelte
+++ src/routes/tickets/+page.svelte
@@ -138,7 +138,7 @@
                 </div>
             {:else}
                 <div class="overflow-x-auto">
-                    <table class="min-w-full divide-y divide-gray-200">
+                    <table class="min-w-full divide-y divide-gray-200 max-h-[75vh]">
                         <thead class="bg-gray-50">
                             <tr>
                                 <th class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
