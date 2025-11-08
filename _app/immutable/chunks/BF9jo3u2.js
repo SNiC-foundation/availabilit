c46cf5551a79b85b6dd7419559ea8655a378f568
MatthiasWentink <matthiaswentink27@gmail.com>
2025-11-08 20:51:28 +0100

    Updates

--- _app/immutable/chunks/BF9jo3u2.js
+++
@@ -1,1 +0,0 @@
-const a="https://availabilit.ia.utwente.nl/api";function s(t){return`${a}${t.startsWith("/")?t:"/"+t}`}function i(t){return`${a}/static${t.startsWith("/")?t:"/"+t}`}export{s as a,i as s};
