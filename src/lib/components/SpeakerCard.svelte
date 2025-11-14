e80e1479759b95669ebd815ab7c32ba5d8328010
MatthiasWentink <matthiaswentink27@gmail.com>
2025-11-14 14:46:04 +0100

    Fix z-index issue for speakers

--- src/lib/components/SpeakerCard.svelte
+++ src/lib/components/SpeakerCard.svelte
@@ -19,7 +19,7 @@
     const editSpeaker = () => goto(`/speakers/edit?id=${id}`)
 </script>
 
-<div class="bg-white shadow-black/50 rounded-3xl overflow-hidden transition-all flex flex-col md:min-h-148 duration-200 {showMore ? 'w-full md:w-112 md:-mb-60 shadow-2xl' : 'w-full md:w-80 shadow-md'}" style="z-index: {10-height};">
+<div class="bg-white shadow-black/50 rounded-3xl overflow-hidden transition-all flex flex-col md:min-h-148 duration-200 {showMore ? 'w-full md:w-112 md:-mb-60 shadow-2xl' : 'w-full md:w-80 shadow-md'}" style="z-index: {height};">
     <div class="relative">
         {#if admin}
             <button class="rounded-lg bg-blue-whale absolute top-2 right-2 shadow shadow-black shadow-lg w-8 h-8" on:click={editSpeaker}>
