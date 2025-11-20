462e333b7cf502427297da421f6984653f8dfe4c
MatthiasWentink <matthiaswentink27@gmail.com>
2025-11-20 12:52:22 +0100

    Updates

---
+++ _app/immutable/chunks/JSKLHevw.js
@@ -0,0 +1,1 @@
+const s="https://api.availabilit.snic.nl/api";function a(t){return`${s}${t.startsWith("/")?t:"/"+t}`}function i(t){return`${s}/static${t.startsWith("/")?t:"/"+t}`}export{a,i as s};
--- _app/immutable/chunks/bWIU4s08.js
+++
@@ -1,1 +0,0 @@
-const s="http://localhost:3001/api";function r(t){return`${s}${t.startsWith("/")?t:"/"+t}`}function a(t){return`${s}/static${t.startsWith("/")?t:"/"+t}`}export{r as a,a as s};
