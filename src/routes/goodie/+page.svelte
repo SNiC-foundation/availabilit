da42a3d0bf60f2c50ccd543f14c5f4aa79ad144c
MatthiasWentink <matthiaswentink27@gmail.com>
2025-11-24 11:05:16 +0100

    Goodie text update

--- src/routes/goodie/+page.svelte
+++ src/routes/goodie/+page.svelte
@@ -5,7 +5,7 @@
 
 <div id="landing_section"
      class="h-[98vh] overflow-hidden relative w-full bg-bottom-backdrop text-white flex items-center justify-center gap-[5vh]">
-    <div class="flex gap-4 items-center flex-col md:flex-row">
+    <div class="flex gap-4 items-center p-4 flex-col md:flex-row">
         <img src={Goodie} alt="Image of the committee goodie of 2025" class="w-64 mx-auto md:m-0"/>
         <div class="flex flex-col items-center">
             <h3 class="text-4xl font-bold text-center">AvailabilIT NFC keychain</h3>
@@ -16,9 +16,9 @@
     </div>
 </div>
 
-<div class="container flex-col py-8">
+<div class="container p-4 flex-col py-8">
     <h1 class="text-4xl font-bold text-blue-whale">What can you use it for?</h1>
-    <p>In principle, you can use it to store any information up to X and pass this information to NFC compatible devices through touch. You can put information on there using an app like TODO. Some examples of how to use it include:</p>
+    <p>In principle, you can use it to store any information up to 137 bytes and pass this information to NFC compatible devices through touch. At the moment, it is empty but you can put information on there using an app such as 'NFC Tools'. Some examples of how to use it include:</p>
     <div class="flex flex-col md:flex-row gap-4 mt-4 w-full">
         <Card iconContainerClass="bg-blue-300 text-blue-700" title="Contact details" icon="fa-solid fa-user" text="Put contact details like your phone number of e-mail address on there to use as 'business card'"/>
         <Card iconContainerClass="bg-cyan-300 text-cyan-700" title="Payment method" icon="fa-solid fa-money-bill" text="Some study associations allow payment through NFC. You can use this keychain as payment method."/>
